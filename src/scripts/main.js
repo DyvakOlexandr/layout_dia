3e31e10173059bc070c96723b6cc4649853c8b30
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-06-17 18:45:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,1 @@
 'use strict';
-const form = document.getElementById('contact-form');
-const input = document.getElementById('email');
-const submitButton = document.getElementById('send-button');
-
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
-  // Здесь можно добавить код для обработки данных формы
-  // ...
-
-  input.value = ''; // Очищаем поле ввода
-});
